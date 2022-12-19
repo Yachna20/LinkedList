09b2df49fbb102698934d5243807c68c4f6fedbd
Yachna20 <thakreyachna20@gmail.com>
2022-12-19 22:27:33 +0530

    [Yachna]-CreateSimpleLinkedlist

---
+++ Linkedlist/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+namespace Linkedlist
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Addnode(56);
+            list.Addnode(30);
+            list.Addnode(70);
+        }
+    }
+}
